adadad4fc25ec3171925757307c693b945019327
cl xu <117554619+sunbeam-kkt@users.noreply.github.com>
2023-08-03 11:24:56 +0800

    ntu120_gen_bone_data.py

--- data_gen/ntu120_gen_bone_data.py
+++ data_gen/ntu120_gen_bone_data.py
@@ -1,5 +1,4 @@
-# 操作人员：徐成龙
-# 操作时间：2023/5/28 15:31
+
 import os
 import numpy as np
 from numpy.lib.format import open_memmap
@@ -23,13 +22,13 @@ sets = {'train', 'val'}
 
 # 'ntu/xview', 'ntu/xsub'
 datasets = {'ntu120/xsetup', 'ntu120/xsub'}
-# 骨骼生成
+
 from tqdm import tqdm
 
-for dataset in datasets:     # 人体关键节点的定义及其连接方式   'ntu/xsub','ntu/xview'
+for dataset in datasets:     
     for set in sets:         # 'train', 'val'
         print(dataset, set)
-        data = np.load('../data/{}/{}_data_joint.npy'.format(dataset, set))            # 加载关节数据到data中
+        data = np.load('../data/{}/{}_data_joint.npy'.format(dataset, set))            
         N, C, T, V, M = data.shape
         fp_sp = open_memmap(
             '../data/{}/{}_data_bone.npy'.format(dataset, set),
@@ -38,8 +37,8 @@ for dataset in datasets:     # 人体关键节点的定义及其连接方式   '
             shape=(N, 3, T, V, M))
 
         fp_sp[:, :C, :, :, :] = data
-        for v1, v2 in tqdm(paris[dataset]):      # paris是不同数据集的人关节点的（a,b）连接索引，v1和v2代表的是两个连接的关节的代号
+        for v1, v2 in tqdm(paris[dataset]):     
             if dataset != 'kinetics':
-                v1 -= 1    # 从1~25到0~24
-                v2 -= 1    # 从1到0
-            fp_sp[:, :, :, v1, :] = data[:, :, :, v1, :] - data[:, :, :, v2, :]    # 骨骼的长度和方向信息，二阶信息
+                v1 -= 1    
+                v2 -= 1    
+            fp_sp[:, :, :, v1, :] = data[:, :, :, v1, :] - data[:, :, :, v2, :]    
