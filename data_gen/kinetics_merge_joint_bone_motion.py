716deb9348cc9b1a271c93c6f0667599d4fb6ca9
cl xu <117554619+sunbeam-kkt@users.noreply.github.com>
2023-08-03 15:19:41 +0800

    kinetics_merge_joint_bone_motion.py

--- data_gen/kinetics_merge_joint_bone_motion.py
+++ data_gen/kinetics_merge_joint_bone_motion.py
@@ -13,6 +13,6 @@ for dataset in datasets:
         print(dataset, set)
         data_jpt_motion = np.load('../data/{}/{}_data_joint_motion.npy'.format(dataset, set))
         data_bone_motion = np.load('../data/{}/{}_data_bone_motion.npy'.format(dataset, set))
-        N, C, T, V, M = data_jpt_motion.shape   # (56880， 3， 150， 25， 2)
+        N, C, T, V, M = data_jpt_motion.shape  
         data_jpt_bone_motion = np.concatenate((data_jpt_motion, data_bone_motion), axis=1)  
         np.save('../data/{}/{}_data_joint_bone_motion.npy'.format(dataset, set), data_jpt_bone_motion)
