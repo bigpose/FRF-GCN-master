361dc02a098df1e43924ee7c1a1d47de2179fa4e
cl xu <117554619+sunbeam-kkt@users.noreply.github.com>
2023-08-03 11:25:56 +0800

    ntu120_merge_joint_bone_motion_data.py

--- data_gen/ntu120_merge_joint_bone_motion_data.py
+++ data_gen/ntu120_merge_joint_bone_motion_data.py
@@ -1,5 +1,4 @@
-# operator：xcl
-# operating time：2023/5/28 15:42
+
 import os
 import numpy as np
 
@@ -15,5 +14,5 @@ for dataset in datasets:
         data_jpt_motion = np.load('../data/{}/{}_data_joint_motion.npy'.format(dataset, set))
         data_bone_motion = np.load('../data/{}/{}_data_bone_motion.npy'.format(dataset, set))
         N, C, T, V, M = data_jpt_motion.shape   # (56880， 3， 150， 25， 2)
-        data_jpt_bone_motion = np.concatenate((data_jpt_motion, data_bone_motion), axis=1)  # 将关节运动和骨骼运动的矩阵在第一维度上进行拼接
+        data_jpt_bone_motion = np.concatenate((data_jpt_motion, data_bone_motion), axis=1) 
         np.save('../data/{}/{}_data_joint_bone_motion.npy'.format(dataset, set), data_jpt_bone_motion)
