61d7bbfb84874f1cc3bdfcf7e22632e9cc97c73b
cl xu <117554619+sunbeam-kkt@users.noreply.github.com>
2023-08-03 11:16:48 +0800

    ntu_gendata.py

--- data_gen/ntu_gendata.py
+++ data_gen/ntu_gendata.py
@@ -39,7 +39,7 @@ def read_skeleton_filter(file):
                     'bodyID', 'clipedEdges', 'handLeftConfidence',
                     'handLeftState', 'handRightConfidence', 'handRightState',
                     'isResticted', 'leanX', 'leanY', 'trackingState'
-                ]  # 人ID，剪切的边缘，左手置信度，左手状态，右手置信度，右手状态，已重新记录等第三行的十个数据
+                ]  # Person ID, clipped edge, left-handed confidence, left-handed status, right-handed confidence, right-handed status, re-recorded, etc. ten data in the third row
                 body_info = {
                     k: float(v)
                     for k, v in zip(body_info_key, f.readline().split())
@@ -51,7 +51,7 @@ def read_skeleton_filter(file):
                         'x', 'y', 'z', 'depthX', 'depthY', 'colorX', 'colorY',
                         'orientationW', 'orientationX', 'orientationY',
                         'orientationZ', 'trackingState'
-                    ]  # 每个骨架序列5-29行的信息，每行为一个关节点的信息
+                    ]  # Information for 5-29 rows per skeleton sequence, one joint point per row
                     joint_info = {
                         k: float(v)
                         for k, v in zip(joint_info_key, f.readline().split())
@@ -73,7 +73,7 @@ def get_nonzero_std(s):  # tvc
     return s
 
 
-def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
+def read_xyz(file, max_body=4, num_joint=25):  # Took the first two bodies
     seq_info = read_skeleton_filter(file)
     data = np.zeros((max_body, seq_info['numFrame'], num_joint, 3))
     for n, f in enumerate(seq_info['frameInfo']):
@@ -96,7 +96,7 @@ def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
  #   seq_info = read_skeleton_filter(file)
   #  data = np.zeros((max_body, seq_info['numFrame'], num_joint, 3))
 
-    # 加入随机旋转的数据增强方式
+    # Data enhancement by adding randomized rotation
     #angle = random.uniform(-0.3, 0.3)
     #cos_val = math.cos(angle)
     #sin_val = math.sin(angle)
@@ -106,11 +106,11 @@ def read_xyz(file, max_body=4, num_joint=25):  # 取了前两个body
     #    for m, b in enumerate(f['bodyInfo']):
      #       for j, v in enumerate(b['jointInfo']):
       #          if m < max_body and j < num_joint:
-                    # 对当前关节的xyz坐标进行随机旋转
+                    # Randomized rotation of the xyz coordinates of the current joints
        #             x, y, z = v['x'], v['y'], v['z']
         #            x_new = x * cos_val + z * sin_val
          #           z_new = -x * sin_val + z * cos_val
-                    # 再对随即旋转后的坐标进行随即缩放
+                    # Then the coordinates after the on-the-fly rotation are scaled on-the-fly
                     #x_new = x_new * (1 + coefficient)
                     #y = y * (1 + coefficient)
                     #z_new = z_new * (1 + coefficient)
@@ -169,16 +169,16 @@ def gendata(data_path, out_path, ignored_sample_path=None, benchmark='xview', pa
 
     fp = np.zeros((len(sample_label), 3, max_frame, num_joint, max_body_true), dtype=np.float32)  # N, C, T, V, M
 
-    for i, s in enumerate(tqdm(sample_name)):    # i为序号，s为标签名称
+    for i, s in enumerate(tqdm(sample_name)):    # i is the serial number and s is the label name
         data = read_xyz(os.path.join(data_path, s), max_body=max_body_kinect, num_joint=num_joint)
         fp[i, :, 0:data.shape[1], :, :] = data
 
     fp = pre_normalization(fp)     # N, C, T, V, M  preprocess.py
-    np.save('{}/{}_data_joint.npy'.format(out_path, part), fp)   # 保存关节数据[np.save(data_out_path, fp)]
+    np.save('{}/{}_data_joint.npy'.format(out_path, part), fp)   # Save joint data [np.save(data_out_path, fp)]
 
-# 生成关节数据
+# Generating joint data
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')  # NTU数据转换器
+    parser = argparse.ArgumentParser(description='NTU-RGB-D Data Converter.')  # NTU Data Converter
     parser.add_argument('--data_path', default='../data/nturgbd_raw/nturgb+d_skeletons/')
     parser.add_argument('--ignored_sample_path',
                         default='../data/nturgbd_raw/samples_with_missing_skeletons.txt')
